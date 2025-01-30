2225d2fe18dd99bc28bd3d6d059df3adaa6acc80
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-30 09:30:50 +0100

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Thomas on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
