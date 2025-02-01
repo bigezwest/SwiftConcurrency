c32fbda9e38cf3af9ff14b13f98c42c22261eec0
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-02-01 05:01:49 +0100

    [Chore] Create DoCatchTryThrowsBootcamp SwiftUI View

---
+++ SwiftConcurrency/DoCatchTryThrowsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  DoCatchTryThrowsBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Thomas on 2/1/25.
+//
+
+import SwiftUI
+
+struct DoCatchTryThrowsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoCatchTryThrowsBootcamp()
+}
