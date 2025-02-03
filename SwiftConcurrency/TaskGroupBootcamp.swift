73c03cb87ee28840341d7f096462603a66822d2c
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-02-03 09:54:43 +0100

    [Feature] Create TaskGroupBootcamp SwiftUI View

---
+++ SwiftConcurrency/TaskGroupBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskGroupBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Thomas on 2/3/25.
+//
+
+import SwiftUI
+
+struct TaskGroupBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskGroupBootcamp()
+}
