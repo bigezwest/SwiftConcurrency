3dc529bef65a1d2541338a08ce2d2ccf573bdb15
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-02-06 19:51:34 +0100

    [Feature] Add ObservablkeMacroBootcamp SwiftUi View

---
+++ SwiftConcurrency/ObservableMacroBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  ObservableMacroBootcamp.swift
+//  SwiftConcurrency
+//
+//  Created by Thomas on 2/6/25.
+//
+
+import SwiftUI
+
+actor TitleDatabase {
+    func getNewTitle() -> String {
+        "Some New Title "
+    }
+}
+
+
+@Observable class ObservableMacroViewModel {
+    
+    @ObservationIgnored let database = TitleDatabase()
+    @MainActor var title: String = "Starting Title"
+    
+    func updateTitle() {
+        Task {
+            title = await database.getNewTitle()
+            print(Thread.current)
+        }
+    }
+}
+
+
+struct ObservableMacroBootcamp: View {
+    
+    @State private var viewModel = ObservableMacroViewModel()
+    
+    var body: some View {
+        Text(viewModel.title)
+            .onAppear {
+                viewModel.updateTitle()
+            }
+    }
+}
+
+#Preview {
+    ObservableMacroBootcamp()
+}
